67b21f996828be2e724ed4b5cc93ad25c0db02c9
pank-su <frostkslo1@yandex.ru>
2023-03-09 11:53:38 +0300

    Rework my generator list to function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from random import randrange
+from typing import List
 
-print([randrange(5, 12 * 100) for i in range(22)])
+
+def my_random_list(n: int) -> List[int]:
+    """Генерация списка со случайными числами"""
+    return [randrange(5, 12 * 100) for i in range(n)]
+
+
+if __name__ == '__main__':
+    print(my_random_list(22))
